39d2261bb7cce177407ac44800e9a822040dcf24
SylvainStak <stackersap@gmail.com>
2023-12-20 19:44:36 +0000

    refactor pointer structs

--- 2020/day11/C/part1.c
+++ 2020/day11/C/part1.c
@@ -11,18 +11,20 @@ typedef struct Seats {
   uint32_t columns;
 } Seats;
 
-Seats allocate_seats(uint32_t rows, uint32_t columns) {
-  Seats seats = { NULL, rows, columns };
-  seats.layout = (char **) calloc(rows, sizeof(char *));
+Seats *allocate_seats(uint32_t rows, uint32_t columns) {
+  Seats *seats = (Seats *) malloc(sizeof(Seats));
+  seats->rows = rows;
+  seats->columns = columns;
+  seats->layout = (char **) calloc(rows, sizeof(char *));
   for (size_t i = 0; i < rows; i++)
-    seats.layout[i] = (char *) calloc(columns, sizeof(char));
+    seats->layout[i] = (char *) calloc(columns, sizeof(char));
   return seats;
 }
 
-void fill_layout(Seats seats, const char *content) {
-  for (size_t i = 0; i < seats.rows; i++) {
-    for (size_t j = 0; j < seats.columns; j++) {
-      seats.layout[i][j] = content[i*seats.columns+j];
+void fill_layout(Seats *seats, const char *content) {
+  for (size_t i = 0; i < seats->rows; i++) {
+    for (size_t j = 0; j < seats->columns; j++) {
+      seats->layout[i][j] = content[i*seats->columns+j];
     }
   }
 }
@@ -32,41 +34,42 @@ void free_seats(Seats *seats) {
     free(seats->layout[i]);
   }
   free(seats->layout);
+  free(seats);
 }
 
-bool equal_generations(const Seats gen1, const Seats gen2) {
-  for (size_t i = 0; i < gen1.rows; i++) {
-    for (size_t j = 0; j < gen1.columns; j++) {
-      if (gen1.layout[i][j] != gen2.layout[i][j])
+bool equal_generations(const Seats *gen1, const Seats *gen2) {
+  for (size_t i = 0; i < gen1->rows; i++) {
+    for (size_t j = 0; j < gen1->columns; j++) {
+      if (gen1->layout[i][j] != gen2->layout[i][j])
         return false;
     }
   }
   return true;
 }
 
-uint32_t count_free_seats (Seats seats) {
+uint32_t count_free_seats (Seats *seats) {
   uint32_t count = 0;
-  for (size_t i = 0; i < seats.rows; i++) {
-    for (size_t j = 0; j < seats.columns; j++) {
-      count += seats.layout[i][j] == '#' ? 1 : 0;
+  for (size_t i = 0; i < seats->rows; i++) {
+    for (size_t j = 0; j < seats->columns; j++) {
+      count += seats->layout[i][j] == '#' ? 1 : 0;
     }
   }
   return count;
 }
 
-void copy_layout(Seats dest, Seats src) {
-  for (size_t i = 0; i < src.rows; i++) {
-    for (size_t j = 0; j < src.columns; j++) {
-      dest.layout[i][j] = src.layout[i][j];
+void copy_layout(Seats *dest, Seats *src) {
+  for (size_t i = 0; i < src->rows; i++) {
+    for (size_t j = 0; j < src->columns; j++) {
+      dest->layout[i][j] = src->layout[i][j];
     } 
   }
 }
 
-void calc_next_gen(Seats current, Seats next) {
+void calc_next_gen(Seats *current, Seats *next) {
   copy_layout(next, current);
-  for (size_t i = 0; i < current.rows; i++) {
-    for (size_t j = 0; j < current.columns; j++) {
-      if (current.layout[i][j] == '.') {
+  for (size_t i = 0; i < current->rows; i++) {
+    for (size_t j = 0; j < current->columns; j++) {
+      if (current->layout[i][j] == '.') {
         continue;
       }
 
@@ -76,20 +79,20 @@ void calc_next_gen(Seats current, Seats next) {
           uint32_t w_x = i+x;
           uint32_t w_y = j+y;
           if (
-            0 <= w_x && w_x < current.rows &&
-            0 <= w_y && w_y < current.columns &&
+            0 <= w_x && w_x < current->rows &&
+            0 <= w_y && w_y < current->columns &&
             (w_x != i || w_y != j) &&
-            current.layout[w_x][w_y] == '#'
+            current->layout[w_x][w_y] == '#'
           ) {
             neighbors++;
           }
         }
       }
 
-      if (current.layout[i][j] == 'L' && neighbors == 0) {
-        next.layout[i][j] = '#';
-      } else if (current.layout[i][j] == '#' && neighbors > 3){
-        next.layout[i][j] = 'L';
+      if (current->layout[i][j] == 'L' && neighbors == 0) {
+        next->layout[i][j] = '#';
+      } else if (current->layout[i][j] == '#' && neighbors > 3){
+        next->layout[i][j] = 'L';
       }
     }
   }
@@ -124,21 +127,21 @@ int main() {
   fclose(fp);
   columns = len_seats/rows;
 
-  Seats curr = allocate_seats(rows, columns);
-  Seats next = allocate_seats(rows, columns);
+  Seats *curr = allocate_seats(rows, columns);
+  Seats *next = allocate_seats(rows, columns);
   fill_layout(curr, seats);
   free(seats);
 
   while (!equal_generations(curr, next)) {
     calc_next_gen(curr, next);
-    Seats temp = curr;
+    Seats *temp = curr;
     curr = next;
     next = temp;
   }
 
   printf("%d\n", count_free_seats(curr));
-  free_seats(&curr);
-  free_seats(&next);
+  free_seats(curr);
+  free_seats(next);
   return 0;
 }
 
